25cd1e5cfc603ce64adc18b1579341dc59a2c07a
Jack-Legend <98476675+Jack-Legend@users.noreply.github.com>
2025-03-04 11:06:39 +0800

    version 8: PHP

---
+++ aboutme.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+
+<html lang="zh-TW">
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>盧詳凱簡介</title>
+	<style type="text/css">
+
+* { font-family:"標楷體"; margin-left:auto; margin-right:auto;}
+
+h1 {color:blue; font-size:60px;}
+
+h2 {color:#33ff33; font-size:40px;}
+
+</style>
+
+<script>
+
+function change1() {
+
+document.getElementById("pic").src = "mountain.jpg";
+
+document.getElementById("h2text").innerText = "靜宜資管";
+
+}
+
+function change2() {
+
+document.getElementById("pic").src = "cliff.jpg";
+
+document.getElementById("h2text").innerText = "Siang-Kai Lu";
+
+}
+
+</script>
+
+</head>
+<?php echo date("Y-m-d") ?>
+
+<body>
+	<table width="70%">
+
+<tr>
+
+<td>
+	<img src="Meet-Deimos-Team-Rainbows-latest-addition-1024x576.jpg"width="100%"><img>
+</td>
+
+<td>
+
+	<h1>盧詳凱</h1>
+	<h2>Siang Kai Lu</h2>
+</td>
+
+</tr>
+
+</table>
+
+<table width="70%" border="1">
+	<tr>
+		<td>
+
+	個人網頁：<a href="https://www1.pu.edu.tw/~tcyang">https://www1.pu.edu.tw/~tcyang</a><br>
+	FB：<a href="https://www.facebook.com/SiangKaiLu"
+	target="_blank">https://www.facebook.com/SiangKaiLu</a><br>
+	Tel: <a href="tel:0989285980">0989285980</a><br>
+	E-Mail: <a href="mailto:0604lu@gmail.com">0604lu@gmail.com</a><br>
+
+	</td>
+
+	<td>	
+		OIIAOIIA CAT<br>
+
+	<audio controls>
+
+	<source src="OIIAOIIA CAT but in 4K (Not Actually).mp3" type="audio/mP3">
+
+	</audio><br>
+
+	</td>
+
+	<td>
+			Deimos<br>
+
+	<iframe src="https://www.youtube.com/embed/toO4W1bYtq0" allowfullscreen></iframe>
+
+	</td>
+
+	<td>
+		<iframe height="430" width="350" src="https://bot.dialogflow.com/834d65f0-4635-4908-8e9a-bf580506fa3b"></iframe>
+	</td>
+	
+
+	</tr>
+
+	</table>
+
+
+
+
+
+</body>
+
+</html>
